a4401abc1e24c7ded08cae8efa727c0ee97ff7c1
Ryza4 <rifky fahreza924@gmail.com>
2024-11-04 10:15:24 +0800

    first commit

---
+++ input/H-1.py
@@ -0,0 +1,23 @@
+
+# deklarasi 
+name = "reza"
+
+# Cara pertama: mencetak teks dengan variabel
+print(name, "hello")  # Output: reza hello
+
+# Cara kedua: menggunakan format f-string (lebih direkomendasikan)
+print(f"{name} hello")  # Output: reza hello
+
+# Cara ketiga: menggabungkan string dengan operator +
+print(name + " hello")  # Output: reza hello
+
+# deklarasi 
+Name = ("reza")
+
+print ("hello", Name)
+
+# input nama
+name = input("Masukkan nama: ")
+
+# output nama
+print(f"Halo, {name}!")
